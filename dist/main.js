770acd7c8a3c48f391c8079bc705b15db6367f94
njausteve <njaustevedomino@gmail.com>
2017-04-12 01:16:37 +0530

    merge

---
+++ dist/main.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var greet_1 = require("./greet");
+console.log(greet_1.sayHello("Typescript"));
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { sayHello } from "./greet";
+
+function showHello(divName: string , name:string){
+     const elt = document.getElementById(divName);
+
+     elt.innerText = sayHello(name);
+
+}
+
+showHello("greeting", "Typescript");
